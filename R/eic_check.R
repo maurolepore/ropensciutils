fdcf5dba385bbcac9625c1c8fdb5b6902bb5109f
Mauro Lepore <maurolepore@gmail.com>
2025-02-20 11:43:15 -0300

    polish

---
+++ R/eic_check.R
@@ -0,0 +1,15 @@
+eic_check <- function(handle = "johndoe") {
+  header <- eic_template_header()
+  glue::glue(header, editor_template())
+}
+
+eic_template_header <- function() {
+  "
+  @{handle} thanks for sharing your work with rOpenSci.
+
+  I'll share preliminary checks for discussion with the editorial board.
+
+  @{handle} if any of this helps as feedback, it's a good time to make changes.
+
+  "
+}
