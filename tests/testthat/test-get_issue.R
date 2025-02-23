b7e73e3db8be46ab1821ff8aace3640b8fcc2152
Mauro Lepore <maurolepore@gmail.com>
2025-02-22 23:46:28 -0300

    Skip on ci and if offline

--- tests/testthat/test-get_issue.R
+++ tests/testthat/test-get_issue.R
@@ -1,8 +1,14 @@
 test_that("gets the expected issue", {
+  skip_if_offline()
+  skip_on_ci()
+
   out <- get_comments(168)
   expect_true(hasName(out[[1]], "body"))
 })
 
 test_that("with no number errors", {
+  skip_if_offline()
+  skip_on_ci()
+
   expect_error(get_comments(), "number.*missing")
 })
