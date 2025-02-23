ce58fc5e7c1d0c9c03969266659da61d2df6cd0c
Mauro Lepore <maurolepore@gmail.com>
2025-02-22 23:50:43 -0300

    Make examples robust to lack of internet

--- R/get_comments.R
+++ R/get_comments.R
@@ -5,8 +5,8 @@
 #' @return A list of the issue
 #'
 #' @export
-#' @examples
-#' get_comments(168)
+#' @examplesIf FALSE
+#' get_comments(688)
 get_comments <- function(number) {
   gh::gh(glue::glue(
     "GET /repos/ropensci/software-review/issues/{number}/comments"
